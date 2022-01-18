14d9733e4c4f07e41f98ebf155fe429060cc51dc
Jhonnatan <jhonnatan.macias@globant.com>
2022-01-18 12:33:30 -0500

    Create Project

---
+++ ZemogaTest/ZemogaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZemogaTest
+//
+//  Created by Jhonnatan Macias on 18/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
